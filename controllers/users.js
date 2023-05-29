0784ee4750b62304b674917acead5c053e5874a4
Daria Volodina <dvol_1998@mail.ru>
2023-05-29 23:22:38 +0300

    автотесты 18

--- controllers/users.js
+++ controllers/users.js
@@ -51,8 +51,8 @@ module.exports.updateUserProfile = (req, res) => {
   // eslint-disable-next-line max-len
   User.findByIdAndUpdate(req.user._id, { name: req.body.name, about: req.body.about }, { new: true })
     .then((user) => {
-      if (req.body.name.length >= 2 && req.body.name.length <= 30
-        && req.body.about.length >= 2 && req.body.about.length <= 30) {
+      if ((req.body.name.length >= 2 && req.body.name.length <= 30)
+        || (req.body.about.length >= 2 && req.body.about.length <= 30)) {
         res.send({ data: user });
       }
       res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
