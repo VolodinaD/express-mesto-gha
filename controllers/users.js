7411ad8fac7ba93f8346913d7b9abf34c361d8de
Daria Volodina <dvol_1998@mail.ru>
2023-05-30 20:02:56 +0300

    zbzdb

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,15 @@
 const User = require('../models/user');
+const ValidationError = require('../errors/ValidationError');
+const NotFoundError = require('../errors/NotFoundError');
+const DefaultError = require('../errors/DefaultError');
 
 module.exports.getAllUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(() => {
-      return res.status(500).send({ message: 'Ошибка по умолчанию.' });
+    .catch((err) => {
+      if (err.name === DefaultError.name) {
+        res.status(500).send({ message: 'Ошибка по умолчанию.' });
+      }
     });
 };
 
@@ -18,11 +23,9 @@ module.exports.getUserById = (req, res) => {
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Передан некорректный id пользователя.' });
-      } else if (err.name === 'NotFoundError') {
-        return res.status(404).send({ message: 'Пользователь с указанным id не найден.' });
+        res.status(400).send({ message: 'Передан некорректный id пользователя.' });
       } else {
-        return res.status(500).send({ message: 'Ошибка по умолчанию.' });
+        res.status(500).send({ message: 'Ошибка по умолчанию.' });
       }
     });
 };
@@ -35,10 +38,10 @@ module.exports.createUser = (req, res) => {
       return res.status(201).send({ data: user });
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
-      } else {
-        return res.status(500).send({ message: 'Ошибка по умолчанию.' });
+      if (err.name === ValidationError.name) {
+        res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+      } else if (err.name === DefaultError.name) {
+        res.status(500).send({ message: 'Ошибка по умолчанию.' });
       }
     });
 };
@@ -48,19 +51,19 @@ module.exports.updateUserProfile = (req, res) => {
     name: req.body.name, about: req.body.about,
   }, { new: true, runValidators: true })
     .then((user) => {
-      if (user === null) {
+      if (!user) {
         throw new NotFoundError('Пользователь с указанным id не найден.');
       };
       res.send({ data: user });
       return;
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
-      } else if (err.name === 'NotFoundError') {
-        return res.status(404).send({ message: 'Пользователь с указанным id не найден.' });
-      } else {
-        return res.status(500).send({ message: 'Ошибка по умолчанию.' });
+      if (err.name === ValidationError.name) {
+        res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+      } else if (err.name === NotFoundError.name) {
+        res.status(404).send({ message: 'Пользователь с указанным id не найден.' });
+      } else if (err.name === DefaultError.name) {
+        res.status(500).send({ message: 'Ошибка по умолчанию.' });
       }
     });
 };
@@ -70,18 +73,18 @@ module.exports.updateUserAvatar = (req, res) => {
     new: true, runValidators: true,
   })
     .then((user) => {
-      if (user === null) {
+      if (!user) {
         throw new NotFoundError('Пользователь с указанным id не найден.');
       };
       return res.status(200).send({ data: user });
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Переданы некорректные данные при обновлении аватара.' });
-      } else if (err.name === 'NotFoundError') {
-        return res.status(404).send({ message: 'Пользователь с указанным id не найден.' });
-      } else {
-        return res.status(500).send({ message: 'Ошибка по умолчанию.' });
+      if (err.name === ValidationError.name) {
+        res.status(400).send({ message: 'Переданы некорректные данные при обновлении аватара.' });
+      } else if (err.name === NotFoundError.name) {
+        res.status(404).send({ message: 'Пользователь с указанным id не найден.' });
+      } else if (err.name === DefaultError.name) {
+        res.status(500).send({ message: 'Ошибка по умолчанию.' });
       }
     });
 };
